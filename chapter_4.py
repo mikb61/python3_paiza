3c06cd8f08228aff8fcf67044e338be0a2a31e69
mikb61 <aimer61@icloud.com>
2025-10-24 12:31:03 +0900

    Update chapter_4.py

--- chapter_4.py
+++ chapter_4.py
@@ -71,3 +71,126 @@ enemy = ["スライム", "モンスター", "ゾンビ", "ドラゴン", "魔王
 # ここに、要素をループで表示するコードを記述する
 for i in enemy:
     print(i + "が現れた")
+→
+スライムが現れた
+モンスターが現れた
+ゾンビが現れた
+ドラゴンが現れた
+魔王が現れた
+
+
+# coding: utf-8
+# 要素の合計を計算する
+numbers = [12, 34, 56, 78, 90]
+total = 0
+for num in numbers:
+	# ここに、合計を計算するコードを記述する
+	total += num
+print(total)
+→
+270
+
+
+# split関数
+与えられたデータを指定の記号で分割し、リストとして戻す
+
+line = "勇者,戦士,魔法使い"
+print(line.split(","))
+→
+['勇者', '戦士', '魔法使い']
+
+
+# coding: utf-8
+#英文の単語数を数える
+str = "One cold rainy day when my father was a little boy he met an old alley cat on his street"
+print(len(str.split(" ")))
+→
+20
+
+
+# sys.stdin.readlines関数
+ファイルを全て読み込み、1行毎に処理
+stdin は英語で 「standard input（スタンダード・インプット）」 の略
+
+import sys
+line = sys.stdin.readlines()
+
+
+# 💡 よく一緒に使われる sys モジュールの機能一覧
+| コード例 | 読み方 | 説明 |
+|-----------|---------|------|
+| `sys.stdin` | シス・スタディン | 標準入力（キーボードやファイルなどから読み取る） |
+| `sys.stdout` | シス・スタドアウト | 標準出力（printと同じ） |
+| `sys.stderr` | シス・スタドエラー | エラー出力を扱う |
+| `sys.argv` | シス・アーグブ | コマンドライン引数（プログラム実行時に渡す値） |
+| `sys.exit()` | シス・エグジット | プログラムを途中で終了する |
+| `sys.version` | シス・バージョン | Pythonのバージョン情報を取得する |
+
+---
+
+## 🧠 使用例
+```python
+import sys
+
+# Pythonのバージョンを表示
+print("Python version:", sys.version)
+
+# コマンドライン引数を表示
+print("Arguments:", sys.argv)
+
+# 標準入力から複数行を読み取る
+lines = sys.stdin.readlines()
+print("入力された内容：", lines)
+
+# プログラムを終了する
+# sys.exit()
+
+
+# coding: utf-8
+# 複数行のカンマ区切りデータを出力する
+import sys
+for line in sys.stdin.readlines():
+	# ここに、文字列を分割して、出力するコードを書く
+	enemy = line.rstrip().split(",")
+	print(enemy[0] + "が" + enemy[1] +"匹現れた")
+(入力）
+スライム,30
+モンスター,23
+ゾンビ,15
+ドラゴン,3
+魔王,1
+→
+スライムが30匹現れた
+モンスターが23匹現れた
+ゾンビが15匹現れた
+ドラゴンが3匹現れた
+魔王が1匹現れた
+
+
+# randrange関数
+ランダムな値を生成する
+引数を1つ指定した場合は、ゼロから引数未満の値をランダムに生成する
+
+# coding: utf-8
+# リストを使ったランダムくじ
+# スライム,モンスター,ドラゴン,魔王
+import random
+line = input().rstrip().split(",")
+for enemy in line:
+	print(enemy + "が現れた！")
+#ランダムな数を作る範囲を調べる
+num = len(line)
+print("敵は" + str(num) + "匹")
+#ランダムな数を生成
+attack = random.randrange(num)
+#選んだ後に、「会心の一撃！」と表示
+print(line[attack] + 'に会心の一撃!' + line[attack] + "を倒した！")
+(入力)
+スライム,モンスター,ドラゴン,魔王
+→
+スライムが現れた！
+モンスターが現れた！
+ドラゴンが現れた！
+魔王が現れた！
+敵は4匹
+モンスターに会心の一撃!モンスターを倒した！
