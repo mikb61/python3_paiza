e94f4f6dfb061419e0f20972e67cb17c10526f8d
mikb61 <aimer61@icloud.com>
2025-11-02 15:39:51 +0900

    Create 例外処理.py

---
+++ 例外処理.py
@@ -0,0 +1,95 @@
+# 例外
+コードを実行すると、エラーが発生し、実行が終了する場合がある。このようなとき「例外が発生した」「例外が送出された」という。
+
+a = float("abc")
+
+上記コードを実行すると、ValueError という例外が発生する
+今回は、文字列 abc を浮動小数点数に変換できなかったために発生した
+
+
+# 例外処理
+コード実行時に発生する例外に対しておこなう処理のこと
+例外処理を用意すると、例外によって実行が途中で終了しないようにすることができる
+
+# try文
+try:
+    例外が発生しうる処理
+except 予期される例外名:
+    例外が発生したときにおこなう処理
+
+try:
+    a = float(input())
+except ValueError:
+    print("入力値は数字でなければなりません。")
+
+
+1 つの except 節で、複数の例外を受けるようにすることができる
+コードでは、except 節の例外名を書くところに例外名のタプルを書く
+
+try:
+    a = float(input())
+    b = float(input())
+    print(a / b)
+except (ValueError, ZeroDivisionError):　　←このタプルについては、かっこを省略することはできない！
+    print("入力値が正しくありません。")
+
+
+except 節は複数用意することができる
+except 節を複数用意した場合、発生した例外にマッチする except 節が上から順番に探索され、最初にマッチした except 節に処理が移る
+コードでは、普段 except 節を書くときと同じように書く
+
+try:
+    a = float(input())
+    b = float(input())
+    print(a / b)
+except ValueError:
+    print("入力値は数字でなければなりません。")
+except ZeroDivisionError:
+    print("0 で割ることはできません。")
+
+
+except 節で受ける例外には、その例外クラスの子孫にあたるクラスのものも含まれる
+そのため、except 節で受ける例外のクラスを Exception にすると、ほとんどすべての例外を受けることができる
+
+try:
+    a = float(input())
+    b = float(input())
+    print(a / b)
+except Exception:
+    print("例外が発生しました。")
+
+でも、曖昧でバグにつながりやすいので、できるだけ細かく例外を分類して記述した方がいい
+
+
+例外クラスのインスタンスを生成することができる
+e = ValueError("test1", "test2")
+print(e.args)
+例外クラスのインスタンスのメンバ変数 args には、コンストラクタに渡した引数の分だけ要素をもつタプルが代入される
+
+
+# raise 文
+意図的に例外を発生させることができる
+
+raise 文の書き方 1:
+raise 例外クラスのインスタンス
+
+例:
+raise ValueError("0 ≦ x ≦ 10 の整数を指定してください。")
+
+例外クラスに渡した引数の値（= 例外クラスのインスタンスのメンバ変数 args の値）は、エラーメッセージに使われる
+
+raise 文の書き方 2:
+raise 例外クラス名
+
+例:
+raise ValueError
+
+この場合、メンバ変数 args の値を設定できないため、「なぜエラーが発生したか」を詳細に伝えることができない
+
+
+# 独自の例外クラスを定義
+Exception クラスを継承してクラスを定義すると、独自の例外クラスを定義することができる
+class InputValueError(Exception):
+    pass
+
+raise InputValueError("入力値は整数値でなければなりません。")
